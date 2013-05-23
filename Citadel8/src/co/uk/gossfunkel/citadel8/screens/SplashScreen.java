f9a7d5ec4c4e466634796724e41a594d638c519d
Andrew Goss <gossfunkel@gmail.com>
2013-05-23 17:08:19 +0100

    splashscreen

--- Citadel8/src/co/uk/gossfunkel/citadel8/screens/SplashScreen.java
+++ Citadel8/src/co/uk/gossfunkel/citadel8/screens/SplashScreen.java
@@ -1,12 +1,35 @@
 package co.uk.gossfunkel.citadel8.screens;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SplashScreen implements Screen {
+	
+	private Texture splashTexture;
+	private Sprite splashSprite;
+	private SpriteBatch batch;
+	private Game game;
+	
+	public SplashScreen(Game game) {
+		this.game = game;
+	}
 
 	@Override
 	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
+		batch.begin();
+		{
+			splashSprite.draw(batch);
+		}
+		batch.end();
 	}
 
 	@Override
@@ -17,7 +40,14 @@ public class SplashScreen implements Screen {
 
 	@Override
 	public void show() {
+		splashTexture = new Texture("data/splashscreen.png");
+		splashTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
+		splashSprite = new Sprite(splashTexture);
+		splashSprite.setOrigin(splashSprite.getWidth()/2, splashSprite.getHeight()/2);
+		splashSprite.setPosition(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		batch = new SpriteBatch();
 	}
 
 	@Override
